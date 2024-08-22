cd0788b0a03399087f57fb2d9d01a01cb35907ae
niteshrawat0401 <niteshrawat0401@gmail.com>
2024-08-22 22:31:12 +0530

    changes in routes

--- src/app/products/[productId]/page.jsx
+++ src/app/products/[productId]/page.jsx
@@ -1,30 +1,43 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 
-export default function ProductDeatils({params}){
-    const [singleData, setSingleData] = useState([]);
-    
-    const getSingleData =async()=>{
-        try {
-            const res = await fetch(`https://fakestoreapi.com/products/${params.productId}`);
-            const data = await res.json();
-            setSingleData(data)
-            console.log(data)
+export default function ProductDeatils({ params }) {
+  const [singleData, setSingleData] = useState([]);
 
-        } catch (error) {
-            console.log(error)
-        }
+  const getSingleData = async () => {
+    try {
+      const res = await fetch(
+        `https://fakestoreapi.com/products/${params.productId}`
+      );
+      const data = await res.json();
+      setSingleData(data);
+      // console.log(data)
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(()=>{
-        getSingleData()
-    },[])
-    return(
+  useEffect(() => {
+    getSingleData();
+  }, []);
+  return (
+    <div>
+        <h2>Product Deatils</h2>
+      <div
+        style={{
+          border: "1px solid grey",
+          display: "flex",
+          alignItems: "center",
+          background: '#eef9f9'
+        }}
+      >
+        <img width={100} src={singleData.image} />
         <div>
-             <div style={{border: '1px solid grey', display: 'flex', alignItems: 'center', cursor: 'pointer'}}>
-                        <img width={100} src={singleData.image}/>
-                        <p>{singleData.title}</p>
-                    </div>
+        <p>  Title: {singleData.title}</p>
+        <p>  Price: {singleData.price}</p>
+        <p>  Category: {singleData.category}</p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,40 +1,63 @@
-"use client"; // This is a client component 👈🏽
+"use client";
 
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-export default function ProductList(){
-    const [data, setData] = useState([]);
-    const router = useRouter();
+export default function ProductList() {
+  const [data, setData] = useState([]);
+  const [isHovered, setIsHovered] = useState(false);
+  const router = useRouter();
 
-    const getData = async ()=>{
-        try {
-            const res = await fetch('https://fakestoreapi.com/products')
-            const data = await res.json();
-            setData(data)
-            // console.log(data)
-        } catch (error) {
-            console.log(error)
-        }
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+  const buttonStyle = {
+    background: isHovered ? '#eef9f9' : '#f7f6f3',
+    border: '1px solid black',
+    display: "flex",
+    alignItems: "center",
+    cursor: "pointer",
+    transition: 'background 0.3s',
+    cursor: 'pointer',
+  };
+
+  const getData = async () => {
+    try {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      setData(data);
+      // console.log(data)
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(()=>{
-        getData()
-    },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
-    return(
-        <div>
-            <p>product List</p>
-            {data?.map((e)=>{
-                return(
-                    <div onClick={()=>router.push(`/products/${e.id}`)} key={e.id} style={{border: '1px solid grey', display: 'flex', alignItems: 'center', cursor: 'pointer'}}>
-                        <img width={100} src={e.image}/>
-                        <p>{e.title}</p>
-                    </div>
-                )
-            })
-            }
-        </div>
-    )
+  return (
+    <div>
+      <h2>Product List</h2>
+      {data?.map((e) => {
+        return (
+          <div
+            onClick={() => router.push(`/products/${e.id}`)}
+            key={e.id}
+            style={buttonStyle}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+          >
+            <img width={100} src={e.image} />
+            <p> Title: {e.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
