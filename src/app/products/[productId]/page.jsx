90ebdd92aaad62164021cbd9a91ca2f88b53cb92
niteshrawat0401 <niteshrawat0401@gmail.com>
2024-08-22 20:35:49 +0530

    get data from api

--- src/app/products/[productId]/page.jsx
+++ src/app/products/[productId]/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function ProductDeatils({params}){
+    console.log(params)
     return(
         <div>
             <p>product page {params.productId}</p>
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+"use client"; // This is a client component 👈🏽
+
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function ProductList(){
+    const [data, setData] = useState([]);
+    const router = useRouter();
+
+    const getData = async ()=>{
+        try {
+            const res = await fetch('https://fakestoreapi.com/products')
+            const data = await res.json();
+            setData(data)
+            // console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(()=>{
+        getData()
+    },[])
+
     return(
         <div>
-            <p>product all page</p>
-            <p>1</p>
-            <p>2</p>
-            <p>3</p>
+            <p>product List</p>
+            {data?.map((e)=>{
+                return(
+                    <div onClick={()=>router.push(`/products/${e.id}`)} key={e.id} style={{border: '1px solid grey', display: 'flex', alignItems: 'center', cursor: 'pointer'}}>
+                        <img width={100} src={e.image}/>
+                        <p>{e.title}</p>
+                    </div>
+                )
+            })
+            }
         </div>
     )
 }
