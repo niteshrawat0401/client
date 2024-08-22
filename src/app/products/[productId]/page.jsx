bd8ca465f8739836e7f2f4149a59ae7d997ad902
niteshrawat0401 <niteshrawat0401@gmail.com>
2024-08-22 20:42:16 +0530

    get singledata from api

--- src/app/products/[productId]/page.jsx
+++ src/app/products/[productId]/page.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 
 export default function ProductDeatils({params}){
-    console.log(params)
+    const [singleData, setSingleData] = useState([]);
+    
+    const getSingleData =async()=>{
+        try {
+            const res = await fetch(`https://fakestoreapi.com/products/${params.productId}`);
+            const data = await res.json();
+            setSingleData(data)
+            console.log(data)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(()=>{
+        getSingleData()
+    },[])
     return(
         <div>
-            <p>product page {params.productId}</p>
+             <div style={{border: '1px solid grey', display: 'flex', alignItems: 'center', cursor: 'pointer'}}>
+                        <img width={100} src={singleData.image}/>
+                        <p>{singleData.title}</p>
+                    </div>
         </div>
     )
 }
